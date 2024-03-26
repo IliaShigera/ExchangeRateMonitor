46fbb9a7b7b61604e2f9332379de14be1d8aa1c0
Ilia <ShigeraIlia@gmail.com>
2024-03-26 18:39:57 -0400

    add ExchangeRateData with its entity config

---
+++ src/ExchangeRateMonitor.Core/ExchangeRateData.cs
@@ -0,0 +1,25 @@
+﻿namespace ExchangeRateMonitor.Core;
+
+public sealed class ExchangeRateData
+{
+    public ExchangeRateData(int timestamp, string baseCurrency, string targetCurrency, DateTime dateUtc, decimal rate)
+    {
+        Timestamp = timestamp;
+        BaseCurrency = baseCurrency;
+        TargetCurrency = targetCurrency;
+        DateUtc = dateUtc;
+        Rate = rate;
+    }
+
+    private ExchangeRateData()
+    {
+        // EF only
+    }
+
+    public Guid Id { get; private set; }
+    public string BaseCurrency { get; init; }
+    public string TargetCurrency { get; init; }
+    public int Timestamp { get; init; }
+    public DateTime DateUtc { get; init; }
+    public decimal Rate { get; init; }
+}
---
+++ src/ExchangeRateMonitor.DAL.EF/EntityConfig/ExchangeRateEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace ExchangeRateMonitor.DAL.EF.EntityConfig;
+
+internal sealed class ExchangeRateEntityTypeConfiguration : IEntityTypeConfiguration<ExchangeRateData>
+{
+    public void Configure(EntityTypeBuilder<ExchangeRateData> builder)
+    {
+        builder.ToTable("ExchangeRates");
+
+        builder.HasKey(er => er.Id);
+        
+        builder.Property(er => er.BaseCurrency).IsRequired();
+        builder.Property(er => er.TargetCurrency).IsRequired();
+        builder.Property(er => er.Timestamp).IsRequired();
+        builder.Property(er => er.DateUtc).IsRequired();
+        builder.Property(er => er.Rate).IsRequired();
+    }
+}
