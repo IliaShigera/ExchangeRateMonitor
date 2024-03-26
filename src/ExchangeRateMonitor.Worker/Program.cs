b07304d96e462b80cc7cefbf60a43d209119508d
Ilia <ShigeraIlia@gmail.com>
2024-03-26 11:14:58 -0400

    setup

---
+++ src/ExchangeRateMonitor.Worker/Program.cs
@@ -0,0 +1,36 @@
+﻿try
+{
+    var builder = new ConfigurationBuilder();
+    
+    BuildConfig(builder);
+    
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((context, services) => AddServices(context.Configuration, services))
+        .UseSerilog((context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration))
+        .Build();
+    
+    Log.Information("Starting up!");
+}
+catch (Exception ex)
+{
+    Log.Error("The following {Exception} was thrown during application startup", ex);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+internal static partial class Program // startup
+{
+    private static IConfiguration BuildConfig(IConfigurationBuilder builder)
+    {
+        return builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+    }
+
+    private static void AddServices(IConfiguration configuration, IServiceCollection services)
+    {
+    }
+}
